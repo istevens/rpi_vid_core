a97b506682cbed04a4a5611893c750a15ed43726
bryancole <none@none>
2012-05-22 08:11:44 +0100

    builds and mostly works. es.RegisterDrawFunc is still broken though

--- test_vid_core.py
+++ test_vid_core.py
@@ -5,6 +5,15 @@ import es
 
 ctx = es.Context()
 
+es.InitContext(ctx)
+ES_WINDOW_RGB=0
+es.CreateWindow(ctx, "Hello Triangle", 320, 240, ES_WINDOW_RGB )
+
+def func(ctx):
+    print "foo", ctx
+#es.RegisterDrawFunc(ctx, func)
+es.MainLoop(ctx)
 print "end"
+
 
 
