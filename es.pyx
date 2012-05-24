c425ee917740cb35818121a97f2e713dac962c70
bryancole <none@none>
2012-05-24 09:18:26 +0100

    Fixed seg-fault when registering draw-func.

--- es.pyx
+++ es.pyx
@@ -2,11 +2,25 @@
 from libc.stdlib cimport malloc, free
 
 cdef extern from "./esUtil.h":
-    ctypedef struct ESContext:
-        void *userData
     ctypedef unsigned char    GLboolean
     ctypedef int              GLint
     ctypedef unsigned int     GLuint
+    ctypedef void *EGLNativeWindowType
+    ctypedef void *EGLDisplay
+    ctypedef void *EGLContext
+    ctypedef void *EGLSurface
+    ctypedef struct ESContext:
+        void *userData
+        GLint width
+        GLint height
+        EGLNativeWindowType  hWnd
+        EGLDisplay  eglDisplay
+        EGLContext  eglContext
+        EGLSurface  eglSurface
+        void (*drawFunc) #( struct _escontext * )
+        void (*keyFunc) #( struct _escontext *, unsigned char, int, int )
+        void (*updateFunc) #( struct _escontext *, float deltaTime )
+    
     ctypedef void (*drawFunc)(ESContext *)
     
     void esInitContext ( ESContext *esContext )
@@ -19,23 +33,24 @@ cdef drawFunc this_callback
 
 cdef class Context:
     cdef ESContext *_c_esctx
-    cdef object draw_func
+    cdef public object draw_func
     def __cinit__(self):
         self._c_esctx = <ESContext*>malloc(sizeof(ESContext))
+        esInitContext(self._c_esctx)
         self._c_esctx.userData = <void*>self
         if self._c_esctx is NULL:
             raise MemoryError("Couldn't assign ESContext memory")
-            
+
         def on_draw(ctx):
             print "drawing...", ctx
         self.draw_func = on_draw
             
     def __dealloc__(self):
         free(self._c_esctx)
-
-
-def InitContext(Context ctx):
-    esInitContext(ctx._c_esctx)
+        
+    def get_self(self):
+        return <object>(self._c_esctx.userData)
+        
         
 def CreateWindow(Context ctx, char *title, GLint width, GLint height, GLuint flags):
     if esCreateWindow(ctx._c_esctx, title, width, height, flags) != 1:
@@ -48,7 +63,8 @@ def RegisterDrawFunc(Context ctx, object func):
     esRegisterDrawFunc(ctx._c_esctx, _draw)
     
 cdef void _draw(ESContext *ctx):
-    pyctx = <object>ctx.userData
+    cdef Context pyctx
+    pyctx = <Context>(ctx.userData)
     pyctx.draw_func(pyctx)
         
 def MainLoop(Context ctx):
--- test_vid_core.py
+++ test_vid_core.py
@@ -5,13 +5,19 @@ import es
 
 ctx = es.Context()
 
-es.InitContext(ctx)
+self = ctx.get_self()
+print "check:", self is ctx
+
+#es.InitContext(ctx)
 ES_WINDOW_RGB=0
 es.CreateWindow(ctx, "Hello Triangle", 320, 240, ES_WINDOW_RGB )
 
 def func(ctx):
     print "foo", ctx
-#es.RegisterDrawFunc(ctx, func)
+es.RegisterDrawFunc(ctx, func)
+
+print "draw_func", ctx.draw_func
+
 es.MainLoop(ctx)
 print "end"
 
