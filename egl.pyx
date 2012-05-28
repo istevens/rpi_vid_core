eace51518701a560d2a898fec5fd12fce1ee67d6
bryancole <none@none>
2012-05-28 22:18:44 +0100

    started adding EGL constants - incomplete

--- egl.pyx
+++ egl.pyx
@@ -66,11 +66,160 @@ cdef extern from "/opt/vc/include/EGL/egl.h":
     EGLBoolean eglCopyBuffers(EGLDisplay dpy, EGLSurface surface,
 			  EGLNativePixmapType target)
 
-class CONST:
+class _constants:
+    #/* EGL Versioning */
+    #define EGL_VERSION_1_0			1
+    #define EGL_VERSION_1_1			1
+    #define EGL_VERSION_1_2			1
+    #define EGL_VERSION_1_3			1
+    #define EGL_VERSION_1_4			1
+
+    /* EGL Enumerants. Bitmasks and other exceptional cases aside, most
+     * enums are assigned unique values starting at 0x3000.
+     */
+
+    /* EGL aliases */
+    #define EGL_FALSE			0
+    #define EGL_TRUE			1
+
+    /* Out-of-band handle values */
+    #define EGL_DEFAULT_DISPLAY		((EGLNativeDisplayType)0)
+    #define EGL_NO_CONTEXT			((EGLContext)0)
+    #define EGL_NO_DISPLAY			((EGLDisplay)0)
+    #define EGL_NO_SURFACE			((EGLSurface)0)
+
+    /* Out-of-band attribute value */
+    #define EGL_DONT_CARE			((EGLint)-1)
+
+    /* Errors / GetError return values */
+    #define EGL_SUCCESS			0x3000
+    #define EGL_NOT_INITIALIZED		0x3001
+    #define EGL_BAD_ACCESS			0x3002
+    #define EGL_BAD_ALLOC			0x3003
+    #define EGL_BAD_ATTRIBUTE		0x3004
+    #define EGL_BAD_CONFIG			0x3005
+    #define EGL_BAD_CONTEXT			0x3006
+    #define EGL_BAD_CURRENT_SURFACE		0x3007
+    #define EGL_BAD_DISPLAY			0x3008
+    #define EGL_BAD_MATCH			0x3009
+    #define EGL_BAD_NATIVE_PIXMAP		0x300A
+    #define EGL_BAD_NATIVE_WINDOW		0x300B
+    #define EGL_BAD_PARAMETER		0x300C
+    #define EGL_BAD_SURFACE			0x300D
+    #define EGL_CONTEXT_LOST		0x300E	/* EGL 1.1 - IMG_power_management */
+
+    /* Reserved 0x300F-0x301F for additional errors */
+
+    /* Reserved 0x3041-0x304F for additional config attributes */
+
+    /* Config attribute values */
+    #define EGL_SLOW_CONFIG			0x3050	/* EGL_CONFIG_CAVEAT value */
+    #define EGL_NON_CONFORMANT_CONFIG	0x3051	/* EGL_CONFIG_CAVEAT value */
+    #define EGL_TRANSPARENT_RGB		0x3052	/* EGL_TRANSPARENT_TYPE value */
+    #define EGL_RGB_BUFFER			0x308E	/* EGL_COLOR_BUFFER_TYPE value */
+    #define EGL_LUMINANCE_BUFFER		0x308F	/* EGL_COLOR_BUFFER_TYPE value */
+
+    /* More config attribute values, for EGL_TEXTURE_FORMAT */
+    #define EGL_NO_TEXTURE			0x305C
+    #define EGL_TEXTURE_RGB			0x305D
+    #define EGL_TEXTURE_RGBA		0x305E
+    #define EGL_TEXTURE_2D			0x305F
+
+    /* Config attribute mask bits */
+    #define EGL_PBUFFER_BIT			0x0001	/* EGL_SURFACE_TYPE mask bits */
+    #define EGL_PIXMAP_BIT			0x0002	/* EGL_SURFACE_TYPE mask bits */
+    #define EGL_WINDOW_BIT			0x0004	/* EGL_SURFACE_TYPE mask bits */
+    #define EGL_VG_COLORSPACE_LINEAR_BIT	0x0020	/* EGL_SURFACE_TYPE mask bits */
+    #define EGL_VG_ALPHA_FORMAT_PRE_BIT	0x0040	/* EGL_SURFACE_TYPE mask bits */
+    #define EGL_MULTISAMPLE_RESOLVE_BOX_BIT 0x0200	/* EGL_SURFACE_TYPE mask bits */
+    #define EGL_SWAP_BEHAVIOR_PRESERVED_BIT 0x0400	/* EGL_SURFACE_TYPE mask bits */
+
+    #define EGL_OPENGL_ES_BIT		0x0001	/* EGL_RENDERABLE_TYPE mask bits */
+    #define EGL_OPENVG_BIT			0x0002	/* EGL_RENDERABLE_TYPE mask bits */
+    #define EGL_OPENGL_ES2_BIT		0x0004	/* EGL_RENDERABLE_TYPE mask bits */
+    #define EGL_OPENGL_BIT			0x0008	/* EGL_RENDERABLE_TYPE mask bits */
+
+    /* QueryString targets */
+    #define EGL_VENDOR			0x3053
+    #define EGL_VERSION			0x3054
+    #define EGL_EXTENSIONS			0x3055
+    #define EGL_CLIENT_APIS			0x308D
+
+    /* QuerySurface / SurfaceAttrib / CreatePbufferSurface targets */
+    #define EGL_HEIGHT			0x3056
+    #define EGL_WIDTH			0x3057
+    #define EGL_LARGEST_PBUFFER		0x3058
+    #define EGL_TEXTURE_FORMAT		0x3080
+    #define EGL_TEXTURE_TARGET		0x3081
+    #define EGL_MIPMAP_TEXTURE		0x3082
+    #define EGL_MIPMAP_LEVEL		0x3083
+    #define EGL_RENDER_BUFFER		0x3086
+    #define EGL_VG_COLORSPACE		0x3087
+    #define EGL_VG_ALPHA_FORMAT		0x3088
+    #define EGL_HORIZONTAL_RESOLUTION	0x3090
+    #define EGL_VERTICAL_RESOLUTION		0x3091
+    #define EGL_PIXEL_ASPECT_RATIO		0x3092
+    #define EGL_SWAP_BEHAVIOR		0x3093
+    #define EGL_MULTISAMPLE_RESOLVE		0x3099
+
+    /* EGL_RENDER_BUFFER values / BindTexImage / ReleaseTexImage buffer targets */
+    #define EGL_BACK_BUFFER			0x3084
+    #define EGL_SINGLE_BUFFER		0x3085
+
+    /* OpenVG color spaces */
+    #define EGL_VG_COLORSPACE_sRGB		0x3089	/* EGL_VG_COLORSPACE value */
+    #define EGL_VG_COLORSPACE_LINEAR	0x308A	/* EGL_VG_COLORSPACE value */
+
+    /* OpenVG alpha formats */
+    #define EGL_VG_ALPHA_FORMAT_NONPRE	0x308B	/* EGL_ALPHA_FORMAT value */
+    #define EGL_VG_ALPHA_FORMAT_PRE		0x308C	/* EGL_ALPHA_FORMAT value */
+
+    /* Constant scale factor by which fractional display resolutions &
+     * aspect ratio are scaled when queried as integer values.
+     */
+    #define EGL_DISPLAY_SCALING		10000
+
+    /* Unknown display resolution/aspect ratio */
+    #define EGL_UNKNOWN			((EGLint)-1)
+
+    /* Back buffer swap behaviors */
+    #define EGL_BUFFER_PRESERVED		0x3094	/* EGL_SWAP_BEHAVIOR value */
+    #define EGL_BUFFER_DESTROYED		0x3095	/* EGL_SWAP_BEHAVIOR value */
+
+    /* CreatePbufferFromClientBuffer buffer types */
+    #define EGL_OPENVG_IMAGE		0x3096
+
+    /* QueryContext targets */
+    #define EGL_CONTEXT_CLIENT_TYPE		0x3097
+
+    /* CreateContext attributes */
+    #define EGL_CONTEXT_CLIENT_VERSION	0x3098
+
+    /* Multisample resolution behaviors */
+    #define EGL_MULTISAMPLE_RESOLVE_DEFAULT 0x309A	/* EGL_MULTISAMPLE_RESOLVE value */
+    #define EGL_MULTISAMPLE_RESOLVE_BOX	0x309B	/* EGL_MULTISAMPLE_RESOLVE value */
+
+    /* GetCurrentSurface targets */
+    #define EGL_DRAW			0x3059
+    #define EGL_READ			0x305A
+
+    /* WaitNative engines */
+    #define EGL_CORE_NATIVE_ENGINE		0x305B
+
+    /* EGL 1.2 tokens renamed for consistency in EGL 1.3 */
+    #define EGL_COLORSPACE			EGL_VG_COLORSPACE
+    #define EGL_ALPHA_FORMAT		EGL_VG_ALPHA_FORMAT
+    #define EGL_COLORSPACE_sRGB		EGL_VG_COLORSPACE_sRGB
+    #define EGL_COLORSPACE_LINEAR		EGL_VG_COLORSPACE_LINEAR
+    #define EGL_ALPHA_FORMAT_NONPRE		EGL_VG_ALPHA_FORMAT_NONPRE
+    #define EGL_ALPHA_FORMAT_PRE		EGL_VG_ALPHA_FORMAT_PRE
+
+
+
     EGL_OPENGL_ES_API = 0x30A0
     EGL_OPENVG_API	=	0x30A1
     EGL_OPENGL_API	=	0x30A2
-    
+
     EGL_BUFFER_SIZE	=		0x3020
     EGL_ALPHA_SIZE	=		0x3021
     EGL_BLUE_SIZE	=		0x3022
@@ -265,7 +414,6 @@ def CreateWindowSurface(Display dpy, Config config, EGLNativeWindowType win,
     EGLBoolean eglDestroySurface(EGLDisplay dpy, EGLSurface surface)
     EGLBoolean eglQuerySurface(EGLDisplay dpy, EGLSurface surface,
 			   EGLint attribute, EGLint *value)
-    EGLBoolean eglBindAPI(EGLenum api)
     EGLenum eglQueryAPI()
     EGLBoolean eglWaitClient()
     EGLBoolean eglReleaseThread()
