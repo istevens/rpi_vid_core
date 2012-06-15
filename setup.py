76938f2074c61211828bfcf1f8177a2a20591608
bryancole <none@none>
2012-06-15 21:16:56 +0100

    Fixed up setup.py file to some level of working order. Removed some older cruft. Added distribute support

--- setup.py
+++ setup.py
@@ -1,25 +1,48 @@
+import distribute_setup
+distribute_setup.use_setuptools()
 
-from distutils.core import setup
+from setuptools import setup
+#from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+#from Cython.Distutils import build_ext
+
+import subprocess, os
+from glob import glob
+
+source_folder = "vidcore"
+
+source_files = glob(os.path.join(source_folder, "*.pyx"))
+
+for fname in source_files:
+    base, ext = os.path.splitext(fname)
+    cname = "%s.c"%base
+    if os.path.exists(cname) and \
+        os.stat(cname).st_mtime > os.stat(fname).st_mtime:
+        continue
+    try:
+        subprocess.check_call(["cython", fname])
+    except subprocess.CalledProcessError:
+        os.remove(cname)
+    except OSError:
+        print(">>> No Cython version found on the system path")
 
 ext_modules = [
 	Extension(name='vidcore.bcm',
-                    sources=['vidcore/bcm.pyx'],
+                    sources=['vidcore/bcm.c'],
                     extra_compile_args=['-DRPI_NO_X'],
                     libraries=['m','bcm_host'],
                     library_dirs=['/opt/vc/lib'],
                     include_dirs=['.','/opt/vc/include']
                     ),
 	Extension(name='vidcore.egl',
-                    sources=['vidcore/egl.pyx'],
+                    sources=['vidcore/egl.c'],
                     extra_compile_args=['-DRPI_NO_X'],
                     libraries=['GLESv2','EGL','m','bcm_host'],
                     library_dirs=['/opt/vc/lib'],
                     include_dirs=['.','/opt/vc/include']
                     ),
 	Extension(name='vidcore.vg',
-                    sources=['vidcore/vg.pyx'],
+                    sources=['vidcore/vg.c'],
                     extra_compile_args=['-DRPI_NO_X'],
                     libraries=['m','OpenVG'],
                     library_dirs=['/opt/vc/lib'],
@@ -28,8 +51,14 @@ ext_modules = [
 	]
 
 setup(
-  name = 'Raspberry Pi video core APIs',
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules
+  name = 'rpi_vid_core',
+  version="0.1alpha",
+  description="Core hardware-accelerated graphics libraries for the Raspberry Pi: OpenVG, EGL",
+  #cmdclass = {'build_ext': build_ext},
+  ext_modules = ext_modules,
+  author="Bryan Cole",
+  author_email="bryancole.cam@gmail.com",
+  url="https://bitbucket.org/bryancole/rpi_vid_core",
+  zip_safe=True
 )
 
---
+++ vidcore/bcm.h
@@ -0,0 +1,25 @@
+#ifndef __PYX_HAVE__vidcore__bcm
+#define __PYX_HAVE__vidcore__bcm
+
+
+#ifndef __PYX_HAVE_API__vidcore__bcm
+
+#ifndef __PYX_EXTERN_C
+  #ifdef __cplusplus
+    #define __PYX_EXTERN_C extern "C"
+  #else
+    #define __PYX_EXTERN_C extern
+  #endif
+#endif
+
+__PYX_EXTERN_C DL_IMPORT(uint32_t) __pyx_v_7vidcore_3bcm_DISPMANX_PROTECTION_NONE;
+
+#endif /* !__PYX_HAVE_API__vidcore__bcm */
+
+#if PY_MAJOR_VERSION < 3
+PyMODINIT_FUNC initbcm(void);
+#else
+PyMODINIT_FUNC PyInit_bcm(void);
+#endif
+
+#endif /* !__PYX_HAVE__vidcore__bcm */
